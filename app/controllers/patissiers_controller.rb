7a453cac59574d631767b6ddd3dd2273aaad02d2
nomu0719 <kei53415@gmail.com>
2021-05-27 02:43:35 +0000

    お気に入り一覧

--- app/controllers/patissiers_controller.rb
+++ app/controllers/patissiers_controller.rb
@@ -1,6 +1,9 @@
 class PatissiersController < ApplicationController
   def show
     @patissier = Patissier.find(params[:id])
+    @shop = @patissier.shops
+    recommends = Recommend.where(patissier_id: current_patissier.id).pluck(:shop_id)
+    @recommend_list = Shop.find(recommends)  
   end
   def index
     @patissier = current_patissier
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    favorites = Favorite.where(user_id: current_user.id).pluck(:shop_id)
+    @favorite_list = Shop.find(favorites)
   end
 end
